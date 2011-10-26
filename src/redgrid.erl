9ecc1e4f7b46eea5b9a38207b558379368e1e7f0
Jacob Vorreuter <jacob.vorreuter@gmail.com>
2011-10-26 09:10:05 -0700

    increase timeout

--- src/redgrid.erl
+++ src/redgrid.erl
@@ -229,7 +229,7 @@ get_node(Pid, Key) ->
 
 register_node(#state{redis_cli=Pid, ip=Ip, domain=Domain, version=Version, bin_node=BinNode, meta=Meta}) ->
     Key = iolist_to_binary([<<"redgrid:">>, Domain, <<":">>, Version, <<":">>, BinNode]),
-    Cmds = [["HMSET", Key, "ip", Ip | flatten_proplist(Meta)], ["EXPIRE", Key, "6"]],
+    Cmds = [["HMSET", Key, "ip", Ip | flatten_proplist(Meta)], ["EXPIRE", Key, "120"]],
     redo:cmd(Pid, Cmds),
     ok.
 
