99f3ea6cac98d3c4eee1d66cd5f20addea145046
Heinz N. Gies <heinz@licenser.net>
2012-05-07 01:03:37 +0200

    Removed debug print.

--- src/redgrid_sup.erl
+++ src/redgrid_sup.erl
@@ -24,6 +24,5 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    io:format("sup~n"),
     {ok, {{one_for_one, 5, 10}, [?CHILD(redgrid_srv, worker)]}}.
 
